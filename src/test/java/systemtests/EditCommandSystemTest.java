ece903a73d6b5b38884d1026ce7a95f5bd757fc1
Adam <adamwth@hotmail.com>
2018-11-06 18:11:46 +0800

    SystemCommandSystemTest: add test for room select, other system tests: add methods to support testing of display of rooms and checked-in guests

--- src/test/java/systemtests/EditCommandSystemTest.java
+++ src/test/java/systemtests/EditCommandSystemTest.java
@@ -251,20 +251,20 @@ public class EditCommandSystemTest extends ConciergeSystemTest {
      * 4. Asserts that the status bar's sync status changes.<br>
      * 5. Asserts that the command box has the default style class.<br>
      * Verifications 1 and 2 are performed by
-     * {@code ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * @see ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)
-     * @see ConciergeSystemTest#assertSelectedCardChanged(Index)
+     * {@code ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)}.<br>
+     * @see ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)
+     * @see ConciergeSystemTest#assertSelectedGuestCardChanged(Index)
      */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage,
             Index expectedSelectedCardIndex) {
         executeCommand(command);
         expectedModel.updateFilteredGuestList(PREDICATE_SHOW_ALL_GUESTS);
-        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+        assertApplicationDisplaysExpectedGuest("", expectedResultMessage, expectedModel);
         assertCommandBoxShowsDefaultStyle();
         if (expectedSelectedCardIndex != null) {
-            assertSelectedCardChanged(expectedSelectedCardIndex);
+            assertSelectedGuestCardChanged(expectedSelectedCardIndex);
         } else {
-            assertSelectedCardUnchanged();
+            assertSelectedGuestCardUnchanged();
         }
         assertStatusBarUnchangedExceptSyncStatus();
     }
@@ -276,15 +276,15 @@ public class EditCommandSystemTest extends ConciergeSystemTest {
      * 3. Asserts that the browser url, selected card and status bar remain unchanged.<br>
      * 4. Asserts that the command box has the error style.<br>
      * Verifications 1 and 2 are performed by
-     * {@code ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * @see ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     * {@code ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)}.<br>
+     * @see ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)
      */
     private void assertCommandFailure(String command, String expectedResultMessage) {
         Model expectedModel = getModel();
 
         executeCommand(command);
-        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
-        assertSelectedCardUnchanged();
+        assertApplicationDisplaysExpectedGuest(command, expectedResultMessage, expectedModel);
+        assertSelectedGuestCardUnchanged();
         assertCommandBoxShowsErrorStyle();
         assertStatusBarUnchanged();
     }
--- src/test/java/systemtests/SelectCommandSystemTest.java
+++ src/test/java/systemtests/SelectCommandSystemTest.java
@@ -3,12 +3,15 @@ package systemtests;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_GUEST_DISPLAYED_INDEX;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_ROOM_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.commands.SelectCommand.MESSAGE_SELECT_GUEST_SUCCESS;
+import static seedu.address.logic.commands.SelectCommand.MESSAGE_SELECT_ROOM_SUCCESS;
 import static seedu.address.testutil.TestUtil.getLastIndex;
 import static seedu.address.testutil.TestUtil.getMidIndex;
 import static seedu.address.testutil.TypicalGuests.KEYWORD_MATCHING_MEIER;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+import static seedu.address.testutil.TypicalRoomNumbers.ROOM_NUMBER_001;
 
 import org.junit.Test;
 
@@ -28,30 +31,30 @@ public class SelectCommandSystemTest extends ConciergeSystemTest {
          * -> selected
          */
         String command = "   " + SelectCommand.COMMAND_WORD + " " + INDEX_FIRST.getOneBased() + "   ";
-        assertCommandSuccess(command, INDEX_FIRST);
+        assertCommandSuccessGuest(command, INDEX_FIRST);
 
         /* Case: select the last card in the guest list -> selected */
         Index guestCount = getLastIndex(getModel());
         command = SelectCommand.COMMAND_WORD + " " + guestCount.getOneBased();
-        assertCommandSuccess(command, guestCount);
+        assertCommandSuccessGuest(command, guestCount);
 
         /* Case: undo previous selection -> rejected */
         command = UndoCommand.COMMAND_WORD;
         String expectedResultMessage = UndoCommand.MESSAGE_FAILURE;
-        assertCommandFailure(command, expectedResultMessage);
+        assertCommandFailureGuest(command, expectedResultMessage);
 
         /* Case: redo selecting last card in the list -> rejected */
         command = RedoCommand.COMMAND_WORD;
         expectedResultMessage = RedoCommand.MESSAGE_FAILURE;
-        assertCommandFailure(command, expectedResultMessage);
+        assertCommandFailureGuest(command, expectedResultMessage);
 
         /* Case: select the middle card in the guest list -> selected */
         Index middleIndex = getMidIndex(getModel());
         command = SelectCommand.COMMAND_WORD + " " + middleIndex.getOneBased();
-        assertCommandSuccess(command, middleIndex);
+        assertCommandSuccessGuest(command, middleIndex);
 
         /* Case: select the current selected card -> selected */
-        assertCommandSuccess(command, middleIndex);
+        assertCommandSuccessGuest(command, middleIndex);
 
         /* --------------- Perform select operations on the shown filtered guest list ---------------------------- */
 
@@ -60,42 +63,44 @@ public class SelectCommandSystemTest extends ConciergeSystemTest {
          */
         showGuestsWithName(KEYWORD_MATCHING_MEIER);
         int invalidIndex = getModel().getConcierge().getGuestList().size();
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
+        assertCommandFailureGuest(
+                SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
 
         /* Case: filtered guest list, select index within bounds of Concierge and guest list -> selected */
         Index validIndex = Index.fromOneBased(1);
         assertTrue(validIndex.getZeroBased() < getModel().getFilteredGuestList().size());
         command = SelectCommand.COMMAND_WORD + " " + validIndex.getOneBased();
-        assertCommandSuccess(command, validIndex);
+        assertCommandSuccessGuest(command, validIndex);
 
         /* ------------------ Perform invalid select operations on guest list ------------------------------------ */
 
         /* Case: invalid index (0) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + 0,
+        assertCommandFailureGuest(SelectCommand.COMMAND_WORD + " " + 0,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: invalid index (-1) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + -1,
+        assertCommandFailureGuest(SelectCommand.COMMAND_WORD + " " + -1,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: invalid index (size + 1) -> rejected */
         invalidIndex = getModel().getFilteredGuestList().size() + 1;
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
+        assertCommandFailureGuest(
+                SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
 
         /* Case: invalid arguments (alphabets) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " abc",
+        assertCommandFailureGuest(SelectCommand.COMMAND_WORD + " abc",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: invalid arguments (extra argument) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " 1 abc",
+        assertCommandFailureGuest(SelectCommand.COMMAND_WORD + " 1 abc",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: mixed case command word -> rejected */
-        assertCommandFailure("SeLeCt 1", MESSAGE_UNKNOWN_COMMAND);
+        assertCommandFailureGuest("SeLeCt 1", MESSAGE_UNKNOWN_COMMAND);
 
         /* Case: select from empty Concierge -> rejected */
         deleteAllGuests();
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + INDEX_FIRST.getOneBased(),
+        assertCommandFailureGuest(SelectCommand.COMMAND_WORD + " " + INDEX_FIRST.getOneBased(),
                 MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
     }
 
@@ -107,75 +112,70 @@ public class SelectCommandSystemTest extends ConciergeSystemTest {
          * -> selected
          */
         String command = "   " + SelectCommand.COMMAND_WORD + " " + INDEX_FIRST.getOneBased() + "   ";
-        assertCommandSuccess(command, INDEX_FIRST);
+        assertCommandSuccessRoom(command, INDEX_FIRST);
 
         /* Case: select the last card in the room list -> selected */
         Index roomCount = getLastIndex(getModel());
         command = SelectCommand.COMMAND_WORD + " " + roomCount.getOneBased();
-        assertCommandSuccess(command, roomCount);
+        assertCommandSuccessRoom(command, roomCount);
 
         /* Case: undo previous selection -> rejected */
         command = UndoCommand.COMMAND_WORD;
         String expectedResultMessage = UndoCommand.MESSAGE_FAILURE;
-        assertCommandFailure(command, expectedResultMessage);
+        assertCommandFailureRoom(command, expectedResultMessage);
 
         /* Case: redo selecting last card in the list -> rejected */
         command = RedoCommand.COMMAND_WORD;
         expectedResultMessage = RedoCommand.MESSAGE_FAILURE;
-        assertCommandFailure(command, expectedResultMessage);
+        assertCommandFailureRoom(command, expectedResultMessage);
 
         /* Case: select the middle card in the room list -> selected */
         Index middleIndex = getMidIndex(getModel());
         command = SelectCommand.COMMAND_WORD + " " + middleIndex.getOneBased();
-        assertCommandSuccess(command, middleIndex);
+        assertCommandSuccessRoom(command, middleIndex);
 
         /* Case: select the current selected card -> selected */
-        assertCommandSuccess(command, middleIndex);
+        assertCommandSuccessRoom(command, middleIndex);
 
         /* --------------- Perform select operations on the shown filtered room list ---------------------------- */
 
         /* Case: filtered room list, select index within bounds of Concierge but out of bounds of room list
          * -> rejected
          */
-        showGuestsWithName(KEYWORD_MATCHING_MEIER);
+        showRoomWithKeywords(ROOM_NUMBER_001.toString(), null, null, null, null, null, null);
         int invalidIndex = getModel().getConcierge().getGuestList().size();
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
+        assertCommandFailureRoom(SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_ROOM_DISPLAYED_INDEX);
 
-        /* Case: filtered guest list, select index within bounds of Concierge and guest list -> selected */
+        /* Case: filtered room list, select index within bounds of Concierge and room list -> selected */
         Index validIndex = Index.fromOneBased(1);
-        assertTrue(validIndex.getZeroBased() < getModel().getFilteredGuestList().size());
+        assertTrue(validIndex.getZeroBased() < getModel().getFilteredRoomList().size());
         command = SelectCommand.COMMAND_WORD + " " + validIndex.getOneBased();
-        assertCommandSuccess(command, validIndex);
+        assertCommandSuccessRoom(command, validIndex);
 
-        /* ------------------ Perform invalid select operations on guest list ------------------------------------ */
+        /* ------------------ Perform invalid select operations on room list ------------------------------------ */
 
         /* Case: invalid index (0) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + 0,
+        assertCommandFailureRoom(SelectCommand.COMMAND_WORD + " " + 0,
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: invalid index (-1) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + -1,
+        assertCommandFailureRoom(SelectCommand.COMMAND_WORD + " " + -1,
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: invalid index (size + 1) -> rejected */
         invalidIndex = getModel().getFilteredGuestList().size() + 1;
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
+        assertCommandFailureRoom(SelectCommand.COMMAND_WORD + " " + invalidIndex, MESSAGE_INVALID_ROOM_DISPLAYED_INDEX);
 
         /* Case: invalid arguments (alphabets) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " abc",
+        assertCommandFailureRoom(SelectCommand.COMMAND_WORD + " abc",
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: invalid arguments (extra argument) -> rejected */
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " 1 abc",
+        assertCommandFailureRoom(SelectCommand.COMMAND_WORD + " 1 abc",
             String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
         /* Case: mixed case command word -> rejected */
-        assertCommandFailure("SeLeCt 1", MESSAGE_UNKNOWN_COMMAND);
-
-        /* Case: select from empty Concierge -> rejected */
-        deleteAllGuests();
-        assertCommandFailure(SelectCommand.COMMAND_WORD + " " + INDEX_FIRST.getOneBased(),
-            MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
+        assertCommandFailureRoom("SeLeCt 1", MESSAGE_UNKNOWN_COMMAND);
     }
 
     /**
@@ -189,22 +189,55 @@ public class SelectCommandSystemTest extends ConciergeSystemTest {
      * 6. Status bar remains unchanged.<br>
      * Verifications 1, 3 and 4 are performed by
      * {@code ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * @see ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)
-     * @see ConciergeSystemTest#assertSelectedCardChanged(Index)
+     * @see ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)
+     * @see ConciergeSystemTest#assertSelectedRoomCardChanged(Index)
      */
-    private void assertCommandSuccess(String command, Index expectedSelectedCardIndex) {
+    private void assertCommandSuccessGuest(String command, Index expectedSelectedCardIndex) {
         Model expectedModel = getModel();
         String expectedResultMessage = String.format(
-                MESSAGE_SELECT_GUEST_SUCCESS, expectedSelectedCardIndex.getOneBased());
+            MESSAGE_SELECT_GUEST_SUCCESS, expectedSelectedCardIndex.getOneBased());
         int preExecutionSelectedCardIndex = getGuestListPanel().getSelectedCardIndex();
 
         executeCommand(command);
-        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+        assertApplicationDisplaysExpectedGuest("", expectedResultMessage, expectedModel);
 
         if (preExecutionSelectedCardIndex == expectedSelectedCardIndex.getZeroBased()) {
-            assertSelectedCardUnchanged();
+            assertSelectedGuestCardUnchanged();
         } else {
-            assertSelectedCardChanged(expectedSelectedCardIndex);
+            assertSelectedGuestCardChanged(expectedSelectedCardIndex);
+        }
+
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchanged();
+    }
+
+    /**
+     * Executes {@code command} and asserts that the,<br>
+     * 1. Command box displays an empty string.<br>
+     * 2. Command box has the default style class.<br>
+     * 3. Result display box displays the success message of executing select command with the
+     * {@code expectedSelectedCardIndex} of the selected guest.<br>
+     * 4. {@code Storage} and {@code GuestListPanel} remain unchanged.<br>
+     * 5. Selected card is at {@code expectedSelectedCardIndex} and the browser url is updated accordingly.<br>
+     * 6. Status bar remains unchanged.<br>
+     * Verifications 1, 3 and 4 are performed by
+     * {@code ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * @see ConciergeSystemTest#assertApplicationDisplaysExpectedRoom(String, String, Model)
+     * @see ConciergeSystemTest#assertSelectedRoomCardChanged(Index)
+     */
+    private void assertCommandSuccessRoom(String command, Index expectedSelectedCardIndex) {
+        Model expectedModel = getModel();
+        String expectedResultMessage = String.format(
+            MESSAGE_SELECT_ROOM_SUCCESS, expectedSelectedCardIndex.getOneBased());
+        int preExecutionSelectedCardIndex = getRoomListPanel().getSelectedCardIndex();
+
+        executeCommand(command);
+        assertApplicationDisplaysExpectedRoom("", expectedResultMessage, expectedModel);
+
+        if (preExecutionSelectedCardIndex == expectedSelectedCardIndex.getZeroBased()) {
+            assertSelectedRoomCardUnchanged();
+        } else {
+            assertSelectedRoomCardChanged(expectedSelectedCardIndex);
         }
 
         assertCommandBoxShowsDefaultStyle();
@@ -219,15 +252,36 @@ public class SelectCommandSystemTest extends ConciergeSystemTest {
      * 4. {@code Storage} and {@code GuestListPanel} remain unchanged.<br>
      * 5. Browser url, selected card and status bar remain unchanged.<br>
      * Verifications 1, 3 and 4 are performed by
-     * {@code ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * @see ConciergeSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     * {@code ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)}.<br>
+     * @see ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)
      */
-    private void assertCommandFailure(String command, String expectedResultMessage) {
+    private void assertCommandFailureGuest(String command, String expectedResultMessage) {
         Model expectedModel = getModel();
 
         executeCommand(command);
-        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
-        assertSelectedCardUnchanged();
+        assertApplicationDisplaysExpectedGuest(command, expectedResultMessage, expectedModel);
+        assertSelectedGuestCardUnchanged();
+        assertCommandBoxShowsErrorStyle();
+        assertStatusBarUnchanged();
+    }
+
+    /**
+     * Executes {@code command} and asserts that the,<br>
+     * 1. Command box displays {@code command}.<br>
+     * 2. Command box has the error style class.<br>
+     * 3. Result display box displays {@code expectedResultMessage}.<br>
+     * 4. {@code Storage} and {@code GuestListPanel} remain unchanged.<br>
+     * 5. Browser url, selected card and status bar remain unchanged.<br>
+     * Verifications 1, 3 and 4 are performed by
+     * {@code ConciergeSystemTest#assertApplicationDisplaysExpectedGuest(String, String, Model)}.<br>
+     * @see ConciergeSystemTest#assertApplicationDisplaysExpectedRoom(String, String, Model)
+     */
+    private void assertCommandFailureRoom(String command, String expectedResultMessage) {
+        Model expectedModel = getModel();
+
+        executeCommand(command);
+        assertApplicationDisplaysExpectedRoom(command, expectedResultMessage, expectedModel);
+        assertSelectedRoomCardUnchanged();
         assertCommandBoxShowsErrorStyle();
         assertStatusBarUnchanged();
     }
